751ca93f7e6a08439609a46443bfb0bcc08e65b7
Ice129 <91084504+Ice129@users.noreply.github.com>
2022-01-22 17:03:26 +0000

    added the backup

---
+++ BIG RED BUTTON.bat
@@ -0,0 +1,6 @@
+cmd /c Taskkill /PID "pyw.exe" /F
+cmd /c Taskkill /PID "py.exe" /F
+cmd /c Taskkill /PID "python.exe" /F
+echo msgbox "All Python Code Has Been Killed" > %tmp%\tmp.vbs
+cscript /nologo %tmp%\tmp.vbs
+del %tmp%\tmp.vbs
